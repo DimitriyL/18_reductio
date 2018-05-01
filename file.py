4656a026647335bdd03a825e0109ec0c562cbde5
Xavier Chen <xchen5@stuy.edu>
2018-04-30 22:15:38 -0400

    done

--- file.py
+++ file.py
@@ -1,19 +1,27 @@
 import urllib
 
-link = "http://www.gutenberg.org/files/1342/1342-0.txt"
+link = "http://www.gutenberg.org/cache/epub/24336/pg24336.txt"
 f = urllib.urlopen(link)
 myfile = f.read()
 
 wordList = myfile.split()
 
-print wordList
+#print wordList
 
 def freqWord(word):
     newList = [w for w in wordList if w == word]
-    print newList
+    #print newList
     return len(newList)
 
-print freqWord("chair")
+print 'freqWord(\'English\') = ' + str(freqWord("English"))
 
-def mostCommon():
-    
+def groupFreqWord(groupList):
+    newList = [freqWord(w) for w in groupList]
+    return reduce((lambda x,y: x+y), newList)
+
+print 'groupFreqWord([\'The\', \'English\', \'are\']) = ' + str(groupFreqWord(['The', 'English', 'are']))
+
+def mostFreqWord():
+    return reduce((lambda x, y: x if freqWord(x) > freqWord(y) else y), wordList)
+
+print "Most frequent word: " + mostFreqWord()
